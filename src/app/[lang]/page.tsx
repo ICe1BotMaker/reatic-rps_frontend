c0b9b917e6acc242e4be6102fbefc47ff9b31ca9
ICe1BotMaker <ice1github@gmail.com>
2025-06-14 08:43:48 +0900

    [refactor] 홈 화면 수정사항 반영

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { HelpCircleIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -12,7 +13,6 @@ import { useBar } from "@/shared/stores/bar.zustand";
 
 import { ReactComponent as QuestionMarkIcon } from "@/assets/question_mark.svg";
 
-import { ReactComponent as MovieIcon } from "@/assets/button/movie.svg";
 import { ReactComponent as PlayIcon } from "@/assets/button/play.svg";
 
 export default function Home() {
@@ -38,17 +38,11 @@ export default function Home() {
                 <div className="h-full flex flex-col justify-between">
                     <div className="p-[36px_16px]">
                         <div className="relative">
-                            <div className="flex flex-col gap-[8px]">
-                                <p className="font-p_semibold text-[32px] text-white leading-[39px]">
-                                    상금 10만원에
-                                    <br />
-                                    매주 도전해 보세요.
-                                </p>
-
-                                <p className="font-p_regular text-[16px] text-white">
-                                    4주마다 상금이 100만원으로 늘어나요!
-                                </p>
-                            </div>
+                            <p className="font-p_semibold text-[32px] text-white leading-[39px]">
+                                상금 10만원에
+                                <br />
+                                매주 도전해 보세요.
+                            </p>
 
                             <QuestionMarkIcon
                                 className="absolute top-[4px] right-[4px]"
@@ -58,8 +52,17 @@ export default function Home() {
                     </div>
 
                     <div className="pt-[10px] pb-[20px] px-[16px] flex flex-col gap-[8px]">
-                        <Button variants="white_light" Icon={<MovieIcon />}>
-                            영상 보기
+                        <Button
+                            variants="white_light"
+                            Icon={<HelpCircleIcon size={20} />}
+                            onClick={() =>
+                                window.open(
+                                    "https://www.reaticindustry.com/samgakgame-about",
+                                    "_blank"
+                                )
+                            }
+                        >
+                            참가 전 삼각게임에 대해 알아보기
                         </Button>
 
                         <Button
@@ -80,23 +83,13 @@ export default function Home() {
                 <div className="p-[24px] flex flex-col gap-[16px]">
                     <div className="flex flex-col gap-[2px]">
                         <p className="font-p_semibold text-[16px] text-c_black">
-                            회사
+                            주최
                         </p>
 
                         <Link
                             text="리틱인더스트리"
                             link="https://reaticindustry.com"
                         />
-                    </div>
-
-                    <div className="flex flex-col gap-[2px]">
-                        <p className="font-p_semibold text-[16px] text-c_black">
-                            총 관리자
-                        </p>
-
-                        <p className="font-p_regular text-[14px] text-c_black">
-                            유준상
-                        </p>
                     </div>
 
                     <div className="flex flex-col gap-[2px]">
