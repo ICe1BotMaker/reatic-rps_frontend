76177a583ee0e62b09b6ea6908ada8f582abc3b3
ICe1BotMaker <ice1github@gmail.com>
2025-09-04 17:00:15 +0900

    [fix/test] 카카오톡 공유하기 인증

--- src/app/[lang]/game/result/page.tsx
+++ src/app/[lang]/game/result/page.tsx
@@ -110,11 +110,10 @@ export default function GameResult() {
                                     if (Kakao.isInitialized) {
                                         await Kakao.Share.sendCustom({
                                             templateId: 121362,
-                                            serverCallbackArgs: async () => {
-                                                await share({ seasonId });
-                                                await handleStart();
-                                            },
                                         });
+
+                                        await share({ seasonId });
+                                        await handleStart();
                                     } else {
                                         alert("카카오 SDK 오류 발생");
                                     }
