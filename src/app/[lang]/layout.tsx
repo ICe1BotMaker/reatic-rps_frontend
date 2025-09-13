9055fd16f763b5e6348d0f863bc83c9b638c3e92
ICe1BotMaker <ice1github@gmail.com>
2025-09-14 04:32:58 +0900

    [refactor] 서치 콛솔 작업

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,7 +1,23 @@
+import { Metadata } from "next";
+
 import { FramerProvider } from "@/app/[lang]/provider";
 
 import "@/shared/styles/globals.css";
 import "@/shared/styles/fonts.css";
+
+export const metadata: Metadata = {
+    title: {
+        template: "%s | 삼각게임",
+        default: "삼각게임",
+    },
+    description: "게임을 통해 10만원을 쟁취하세요!",
+    openGraph: {
+        siteName: "삼각게임",
+    },
+    twitter: {
+        title: "삼각게임",
+    },
+};
 
 export default function RootLayout({
     children,
@@ -19,9 +35,6 @@ export default function RootLayout({
                     name="naver-site-verification"
                     content="a501d2c85e968ab7d03ecd30ef0dfe3a663e1f98"
                 />
-
-                <title>삼각게임</title>
-                <meta property="og:description" content="samgakgame" />
             </head>
 
             <body>
--- src/app/[lang]/sitemap.ts
+++ src/app/[lang]/sitemap.ts
@@ -4,8 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: "https://삼각게임.com/ko",
-            lastModified: new Date(),
-            changeFrequency: "daily",
+            changeFrequency: "weekly",
             priority: 1,
         },
     ];
